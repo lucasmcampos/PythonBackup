cc991f03cbda9e458219c77b77103d4a9b14446b
lucasmcampos <lucasmcampos@outlook.com>
2021-04-29 20:53:40 -0300

    UPDATE V0.1

--- ScriptBackup.py
+++ ScriptBackup.py
@@ -1,14 +1,20 @@
 import os
 import shutil
+import time
+from tqdm import tqdm
+
 
 nome = input("Digite o nome do funcionário: ")
 pasta_backup = 'C:/Backup' + nome
 os.mkdir(pasta_backup)
 
+
+
 desktop = input("Digite o caminho do Desktop:")
 downs = input("Digite o caminho dos Downloads: ")
-#docs = input("Digite o caminho dos Documentos: ")
+docs = input("Digite o caminho dos Documentos: ")
 imgs = input("Digite o caminho das Imagens: ")
+
 
 
 
@@ -18,12 +24,14 @@ shutil.copytree(src=desktop, dst=copiaDesktop)
 copiaDowns = pasta_backup + '/Downloads'
 shutil.copytree(src=downs, dst=copiaDowns)
 
+copiaDocs = pasta_backup + '/Documentos'
+shutil.copytree(src=docs, dst=copiaDocs)
 
 copiaImgs = pasta_backup + '/Imagens'
 shutil.copytree(src=imgs, dst=copiaImgs)
 
+ 
+print("FINALIZADO!")
 
-
-
-#shutil.copytree(src=imgs, dst=pasta_backup)    
+   
       
