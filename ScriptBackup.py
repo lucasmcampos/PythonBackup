0c25974e3e3eb9613777891c75afc2b01d8517f4
lucasmcampos <lucasmcampos@outlook.com>
2021-04-29 21:28:31 -0300

    UPDATE V0.2

--- ScriptBackup.py
+++ ScriptBackup.py
@@ -1,37 +1,37 @@
 import os
 import shutil
-import time
-from tqdm import tqdm
-
 
 nome = input("Digite o nome do funcionário: ")
 pasta_backup = 'C:/Backup' + nome
-os.mkdir(pasta_backup)
 
+#Função para Criar a pasta do Backup e mostrar a onde ela está.
+def inicio ():
+    os.mkdir(pasta_backup)
+    return print("PASTA CRIADA: " + pasta_backup)   
 
+#Função de copiar os diretórios para pasta do Backup.
+def criacaoPastas():
+    
+    desktop = input("Digite o caminho do Desktop:")
+    copiaDesktop = pasta_backup + '/Área de Trabalho'
+    shutil.copytree(src=desktop, dst=copiaDesktop)
+    
+    downs = input("Digite o caminho dos Downloads: ")
+    copiaDowns = pasta_backup + '/Downloads'
+    shutil.copytree(src=downs, dst=copiaDowns)
+        
+    docs = input("Digite o caminho dos Documentos: ")
+    copiaDocs = pasta_backup + '/Documentos'
+    shutil.copytree(src=docs, dst=copiaDocs)
+        
+    imgs = input("Digite o caminho das Imagens: ")
+    copiaImgs = pasta_backup + '/Imagens'
+    shutil.copytree(src=imgs, dst=copiaImgs)
 
-desktop = input("Digite o caminho do Desktop:")
-downs = input("Digite o caminho dos Downloads: ")
-docs = input("Digite o caminho dos Documentos: ")
-imgs = input("Digite o caminho das Imagens: ")
+    return print("FINALIZADO!")
 
-
-
-
-copiaDesktop = pasta_backup + '/Área de Trabalho'
-shutil.copytree(src=desktop, dst=copiaDesktop)
-
-copiaDowns = pasta_backup + '/Downloads'
-shutil.copytree(src=downs, dst=copiaDowns)
-
-copiaDocs = pasta_backup + '/Documentos'
-shutil.copytree(src=docs, dst=copiaDocs)
-
-copiaImgs = pasta_backup + '/Imagens'
-shutil.copytree(src=imgs, dst=copiaImgs)
-
- 
-print("FINALIZADO!")
-
+#Execução das funções
+inicio() 
+criacaoPastas()
    
       
